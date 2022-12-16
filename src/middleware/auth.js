65cb1fd4a26d350dfdc843926582b3e4a07d4bdf
Anurag-Bharati <anuragbharati26@gmail.com>
2022-12-17 01:27:24 +0545

    Auth Middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const verifyUser = (req, res, next) => {
+    if (!req.headers.authorization) {
+        let err = new Error("Authorization token is missing");
+        res.status(400);
+        return next(err);
+    }
+    token = req.headers.authorization.split(" ")[1];
+    console.log(token);
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) return next(err);
+        console.log(decoded);
+        next();
+    });
+};
+
+module.exports = { verifyUser };
